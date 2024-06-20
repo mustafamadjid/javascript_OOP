4bec644e1eb6f01ff445d7b05e6be8487e3d24f1
mustafamadjid <madjidmustafa@gmail.com>
2024-06-20 09:47:47 +0700

    first commit

---
+++ JS-1-constructor function/constructorFun.js
@@ -0,0 +1,14 @@
+/*
+
+    Constructor Function -> cara lama javascript untuk membuat class
+
+    pembuatan objek menggunakan keyword -> new
+
+*/
+
+// Dalam constructor function, huruf awal nama function harus kapital
+
+function Person() {}
+
+let Budi = new Person();
+let Dirman = new Person();
